6cb912457ed0e1275f64d98dbab63c8bb841a95a
eike-zink <user@eike-zink.de>
2020-04-13 09:50:25 +0200

    Update sketch.js
    Coding Challenges abgeschlossen.

--- CodingChallenges/015.0 Fractal Trees/sketch.js
+++ CodingChallenges/015.0 Fractal Trees/sketch.js
@@ -1,4 +1,7 @@
 let tree = [];
+let leaves = [];
+
+let count = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -10,19 +13,39 @@ function setup() {
 
 function draw() {
   background(51);
+  // Branches anzeigen
   for (let i = 0; i < tree.length; i++) {
-    tree[i].jitter();
+    //tree[i].jitter();
     tree[i].show();
+  }
+
+  // Leaves anzeigen
+  for (let i = 0; i < leaves.length; i++) {
+    fill(255, 0, 100);
+    ellipse(leaves[i].x, leaves[i].y, 8, 8);
   }
 }
 
 function mousePressed() {
-  for (let i = tree.length-1; i >= 0; i--) {
-    if (!tree[i].finished) {
-      let branches = tree[i].newBranches();
-      tree.push(branches.left);
-      tree.push(branches.right);
-      tree[i].finished = true;
+  count++;
+
+  if (count < 5) {
+    for (let i = tree.length-1; i >= 0; i--) {
+      if (!tree[i].finished) {
+        let branches = tree[i].newBranches();
+        tree.push(branches.left);
+        tree.push(branches.right);
+        tree[i].finished = true;
+      }
+    }
+  } else {
+    if (count == 5) {
+      for (let i = 0; i < tree.length; i++) {
+        if (!tree[i].finished) {
+          let leaf = tree[i].end.copy();
+          leaves.push(leaf);
+        }
+      }
     }
   }
 }
