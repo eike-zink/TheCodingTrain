f77d69e4434f95b38b1b7c875632d5bea9b03ac2
eike-zink <user@eike-zink.de>
2020-12-23 21:24:12 +0100

    Sicherung der aktuellen Entwicklung.

--- CodingChallenges/158.0 Shape Classifier/sketch.js
+++ CodingChallenges/158.0 Shape Classifier/sketch.js
@@ -1,20 +1,42 @@
 function setup() {
   createCanvas(64, 64);
+  frameRate(2);
 }
 
 function draw() {
-  background(255);
-  strokeWeight(4);
+  for (let i = 0; i < 3; i++) {
+    //background(255);
+    push();
 
-  let r = random(8, 24);
-  let x = random(r, width-r);
-  let y = random(r, height-r);
+    strokeWeight(4);
+    stroke('red');
 
-  circle(x, y, r*2);
+    let r = random(8, 24);
+    let x = random(r, width-r);
+    let y = random(r, height-r);
 
-  save(`circle${nf(frameCount, 4, 0)}.png`);
+    translate(x, y);
+
+    if (i == 0) {
+      // Kreis
+      circle(0, 0, r*2);
+      //save(`circle${nf(frameCount, 4, 0)}.png`);
+      save('kreis');
+    } else if (i == 1) {
+      // Quadrat
+      rectMode(CENTER);
+      square(0, 0, r*2);
+      //save(`square${nf(frameCount, 4, 0)}.png`);
+      save('quadrat');
+    } else if (i == 2) {
+      // Dreieck
+      triangle(0, -r, r, r, -r, r);
+      //save(`triangle${nf(frameCount, 4, 0)}.png`);
+    }
+    pop();
+  }
 
   if (frameCount == 10) {
-    exit();
+    noLoop();
   }
 }
